eeaf379973528a71bff3f8d6a6f0e42aa7828fdb
Matthew Fleming <matthew.fleming@wanews.com.au>
2024-12-04 19:11:27 +0800

    day4

--- src/commands/q4/index.ts
+++ src/commands/q4/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable unicorn/no-console-spaces */
+/* eslint-disable unicorn/no-for-loop */
 import {Command} from '@oclif/core'
 import {createReadStream} from 'node:fs'
 import {createInterface} from 'node:readline/promises'
@@ -23,34 +25,150 @@ export default class Q4 extends Command {
       input: fileStream,
     })
 
-    const input = []
+    const lines: string[] = []
 
     for await (const line of rl) {
-      input.push(line)
+      lines.push(line)
     }
 
-    const regex = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g
-    let match
-    let sum = 0
-    let on = true
-    for await (const line of input) {
-      while ((match = regex.exec(line))) {
-        const op = match[0]
-        if (op === 'do()') {
-          on = true
-          console.log('on')
-        } else if (op === "don't()") {
-          console.log('off')
-          on = false
-        } else if (on) {
-          const a = Number(match[1])
-          const b = Number(match[2])
+    // const limitX = lines[0].length
+    const limitY = lines.length
 
-          sum += a * b
+    const word = 'XMAS'
+    let count = 0
+
+    function findUpRight(x: number, y: number, word: string) {
+      for (let i = 0; i < word.length; i++) {
+        if (y - i < 0) {
+          return false
         }
+
+        if (lines[y - i][x + i] !== word[i]) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    function findRight(x: number, y: number, word: string) {
+      for (let i = 0; i < word.length; i++) {
+        if (lines[y][x + i] !== word[i]) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    function findDownRight(x: number, y: number, word: string) {
+      for (let i = 0; i < word.length; i++) {
+        if (y + i >= limitY) {
+          return false
+        }
+
+        if (lines[y + i][x + i] !== word[i]) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    function findDown(x: number, y: number, word: string) {
+      for (let i = 0; i < word.length; i++) {
+        if (y + i >= limitY) {
+          return false
+        }
+
+        if (lines[y + i][x] !== word[i]) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    for (let y = 0; y < limitY; y++) {
+      let x = 0
+      while ((x = lines[y].indexOf(word[0], x)) !== -1) {
+        if (findUpRight(x, y, word)) {
+          console.log('Found up right  ', {x, y})
+          count++
+        }
+
+        if (findRight(x, y, word)) {
+          console.log('Found right     ', {x, y})
+          count++
+        }
+
+        if (findDownRight(x, y, word)) {
+          console.log('Found down right', {x, y})
+          count++
+        }
+
+        if (findDown(x, y, word)) {
+          console.log('Found down      ', {x, y})
+          count++
+        }
+
+        x++
+      }
+
+      const reversed = [...word].reverse().join('')
+      while ((x = lines[y].indexOf(reversed[0], x)) !== -1) {
+        if (findUpRight(x, y, reversed)) {
+          console.log('Found down left ', {x, y})
+          count++
+        }
+
+        if (findRight(x, y, reversed)) {
+          console.log('Found left      ', {x, y})
+          count++
+        }
+
+        if (findDownRight(x, y, reversed)) {
+          console.log('Found up left   ', {x, y})
+          count++
+        }
+
+        if (findDown(x, y, reversed)) {
+          console.log('Found up        ', {x, y})
+          count++
+        }
+
+        x++
       }
     }
 
-    this.log(`Sum: ${sum}`)
+    this.log(`XMAS count: ${count}`)
+
+    // Part 2
+    function findX(x: number, y: number) {
+      if (
+        (lines[y - 1][x - 1] === 'M' && lines[y + 1][x + 1] === 'S') ||
+        (lines[y - 1][x - 1] === 'S' && lines[y + 1][x + 1] === 'M')
+      ) {
+        return (
+          (lines[y + 1][x - 1] === 'M' && lines[y - 1][x + 1] === 'S') ||
+          (lines[y + 1][x - 1] === 'S' && lines[y - 1][x + 1] === 'M')
+        )
+      }
+    }
+
+    let x = 0
+    let countX = 0
+    for (let y = 1; y < limitY - 1; y++) {
+      while ((x = lines[y].indexOf('A', x)) !== -1) {
+        if (findX(x, y)) {
+          console.log('Found X', {x, y})
+          countX++
+        }
+
+        x++
+      }
+    }
+
+    this.log(`X count: ${countX}`)
   }
 }
